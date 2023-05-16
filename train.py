551b09b226513f546f2dcd3873511a3012769168
alope107 <auberonlopez@gmail.com>
2023-05-15 23:39:19 -0700

    Formatted code using autopep8 and added documentation on formatting

--- train.py
+++ train.py
@@ -1,41 +1,43 @@
 '''
 Train CoLLAGE Script
 '''
-import sys, argparse
+import sys
+import argparse
 
 import collage.constants as constants
 from collage.runner import create_model
 from collage.settings import TRAINING_PARAMETERS
 
 
-def parse_args( args: list ):
+def parse_args(args: list):
     '''
     Read in arguments
     '''
 
-    parser = argparse.ArgumentParser( usage = 'Train_CoLLAGE.py [optional arguments] input.fasta output_prefix',
-                                      description = 'Train a CoLLAGE codon likelihood model from a provided FASTA ' 
-                                                    'input of coding sequences.',
-                                      formatter_class = argparse.RawTextHelpFormatter, )
-    parser.add_argument( 'input_fasta',
-                         type = str,
-                         help = 'Path to input FASTA file')
-    parser.add_argument( 'prefix',
-                         type = str,
-                         help = 'Model/log file prefix')
-    parser.add_argument( '--validation_fraction',
-                         type = float,
-                         help = 'Fraction of data to withhold for model evaluation' +
-                                '\n\tDefault: ' + format( constants.VALIDATION_FRACTION,'.2f' ),
-                         default = constants.VALIDATION_FRACTION, )
-    parser.add_argument( '--random_seed',
-                         type = int,
-                         help = 'Random seed for splitting training vs evaluation data' +
-                                '\n\tDefault: ' + str(constants.RANDOM_SEED),
-                         default = constants.RANDOM_SEED, )
-    parser.add_argument( '--gpu',
-                         action = 'store_true',
-                         help = 'Use GPU (CUDA) for CoLLAGE training', )
+    parser = argparse.ArgumentParser(usage='Train_CoLLAGE.py [optional arguments] input.fasta output_prefix',
+                                     description='Train a CoLLAGE codon likelihood model from a provided FASTA '
+                                     'input of coding sequences.',
+                                     formatter_class=argparse.RawTextHelpFormatter, )
+    parser.add_argument('input_fasta',
+                        type=str,
+                        help='Path to input FASTA file')
+    parser.add_argument('prefix',
+                        type=str,
+                        help='Model/log file prefix')
+    parser.add_argument('--validation_fraction',
+                        type=float,
+                        help='Fraction of data to withhold for model evaluation' +
+                        '\n\tDefault: ' +
+                        format(constants.VALIDATION_FRACTION, '.2f'),
+                        default=constants.VALIDATION_FRACTION, )
+    parser.add_argument('--random_seed',
+                        type=int,
+                        help='Random seed for splitting training vs evaluation data' +
+                        '\n\tDefault: ' + str(constants.RANDOM_SEED),
+                        default=constants.RANDOM_SEED, )
+    parser.add_argument('--gpu',
+                        action='store_true',
+                        help='Use GPU (CUDA) for CoLLAGE training', )
     parser.add_argument('--epochs',
                         type=int,
                         help='Number of epochs to train for' +
@@ -56,5 +58,3 @@ def main():
 if __name__ == "__main__":
     main()
     sys.exit()
-
-
