f911fecebb3c5a751c07952fb38ec37527f484ce
alope107 <auberonlopez@gmail.com>
2023-11-07 14:51:37 -0800

    Refactor fasta reading.
    - Refactor fasta reading to separate parsing and validation from file
      opening.

--- collage/fasta.py
+++ collage/fasta.py
@@ -16,36 +16,50 @@ def read_fasta(file_name: str | Path,
     '''
     Return dict with keys = names, values = sequences
     '''
-    seq_dict = {}
 
     file_name = Path(file_name)
     file_opener = gzip.open if file_name.suffix == '.gz' else open
     with file_opener(file_name, 'rt') as fasta:
-        for line in fasta:
-            if line[0] == '>':  # New seq
-                name = line[1:].rstrip()
-                if first_word:
-                    name = name.split(' ')[0]  # Uniprot style
-                seq_dict[name] = ''
-            else:
-                seq_dict[name] += line.rstrip()
+        seq_dict = parse_fasta(fasta, first_word)
 
     # Ensure there are sequences in the file
     if not seq_dict:
         raise FileContentsError(f'No sequences found in FASTA file: "{file_name}"')
 
+    validate_seq_dict(seq_dict, override_alphabet_check)
+
+    return seq_dict
+
+
+def validate_seq_dict(seq_dict, override_alphabet_check: bool = False):
     # Infer if alphabet is nucleotide or protein
     if not override_alphabet_check:
         observed_alphabets = set([identify_alphabet(s)
                                  for s in seq_dict.values()])
-        assert 'Unknown' not in observed_alphabets, 'Unknown characters in FASTA file: ' + file_name
+
+        # TODO(auberon): convert these to FileContentsError
+        assert 'Unknown' not in observed_alphabets, 'Unknown characters in FASTA file'
         assert len(
-            observed_alphabets) == 1, 'Both DNA and Protein sequences in FASTA file: ' + file_name
+            observed_alphabets) == 1, 'Both DNA and Protein sequences in FASTA file'
         alphabet = list(observed_alphabets)[0]
 
+    # TODO(auberon): Check with Damien whether this can be
     # Remove sequences with degenerate nucleotides
     # if alphabet == 'DNA':
     #    seq_dict = dict( [ x for x in seq_dict.items() if set( x[1] ) <= set( nucleotides ) ] )
+
+
+def parse_fasta(file_data, first_word: bool):
+    seq_dict = {}
+
+    for line in file_data:
+        if line[0] == '>':  # New seq
+            name = line[1:].rstrip()
+            if first_word:
+                name = name.split(' ')[0]  # Uniprot style
+            seq_dict[name] = ''
+        else:
+            seq_dict[name] += line.rstrip()
 
     return seq_dict
 
